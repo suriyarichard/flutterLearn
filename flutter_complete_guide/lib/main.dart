49ae11d46eaf44e4485a51f79cb957ec1a17d979
suriyarichard <chinnaraj2061971@gmail.com>
2022-03-11 11:46:18 +0530

    question print

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -5,7 +5,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget{
+  void answerQ(){
+    print("this is the answer");
+  }
+  @override
   Widget build(BuildContext context){
-    return MaterialApp(home : Text ("Hello suriya"),);
+    var question =[
+      'what is your name?','what is your age?','what is your favorite color',
+    ];
+    return MaterialApp(
+      home :Scaffold(
+        appBar: AppBar(title:Text( 'MY first app'),),
+        body:Column(
+          children: [
+            Text(question.elementAt(2)),
+            RaisedButton(child: Text("Answer 1"),onPressed:answerQ,),
+            RaisedButton(child: Text("Answer 2"),onPressed:answerQ,),
+            RaisedButton(child: Text("Answer 3"),onPressed:answerQ,),
+            RaisedButton(child: Text("Answer 4"),onPressed:answerQ,),
+          ],
+        )
+      ));
   }
 }
